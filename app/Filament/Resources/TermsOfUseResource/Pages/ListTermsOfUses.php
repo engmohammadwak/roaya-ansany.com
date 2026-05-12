cbfc71fb427f554333b7997a2a9d7015be74c3e9
Mohammad Waleed <38137510+engmohammadwak@users.noreply.github.com>
2026-05-12 09:44:14 +0300

    fix: terms list page - auto redirect to create/edit

--- app/Filament/Resources/TermsOfUseResource/Pages/ListTermsOfUses.php
+++ app/Filament/Resources/TermsOfUseResource/Pages/ListTermsOfUses.php
@@ -3,9 +3,25 @@
 namespace App\Filament\Resources\TermsOfUseResource\Pages;
 
 use App\Filament\Resources\TermsOfUseResource;
+use App\Models\TermsOfUse;
 use Filament\Resources\Pages\ListRecords;
 
 class ListTermsOfUses extends ListRecords
 {
     protected static string $resource = TermsOfUseResource::class;
+
+    public function mount(): void
+    {
+        $record = TermsOfUse::first();
+
+        if ($record) {
+            $this->redirect(
+                TermsOfUseResource::getUrl('edit', ['record' => $record->id])
+            );
+        } else {
+            $this->redirect(
+                TermsOfUseResource::getUrl('create')
+            );
+        }
+    }
 }
