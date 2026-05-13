1579e50ed581c70d58dc79a274b3955ee006beb4
Mohammad Waleed <38137510+engmohammadwak@users.noreply.github.com>
2026-05-13 09:29:11 +0300

    feat: whatsapp float button uses whatsapp_number + whatsapp_text_1 from settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,10 @@
     $cd = App\Models\Setting::get('color_danger',      '#D9384A');
     $cs = App\Models\Setting::get('color_step_active', '#f26b81');
     $locale = app()->getLocale();
+
+    // WhatsApp Float
+    $waFloatNum  = App\Models\Setting::get('whatsapp_number', '');
+    $waFloatText = App\Models\Setting::get('whatsapp_text_1', '');
 @endphp
 <!DOCTYPE html>
 <html lang="{{ $locale }}" dir="{{ $locale === 'ar' ? 'rtl' : 'ltr' }}">
@@ -324,10 +328,19 @@
     })();
     </script>
 
-    @php $wa = App\Models\Setting::get('whatsapp_number','905398863777'); @endphp
-    <a href="https://api.whatsapp.com/send/?phone={{ $wa }}&text={{ urlencode('مرحباً بك') }}" target="_blank" class="whatsapp-float" aria-label="WhatsApp">
+    {{-- WhatsApp Float Button --}}
+    @if($waFloatNum)
+    @php
+        $waFloatUrl = 'https://wa.me/' . $waFloatNum;
+        if ($waFloatText) {
+            $waFloatUrl .= '?text=' . rawurlencode($waFloatText);
+        }
+    @endphp
+    <a href="{{ $waFloatUrl }}" target="_blank" class="whatsapp-float" aria-label="WhatsApp">
         <svg fill="#fff" width="30px" height="30px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M26.576 5.363c-2.69-2.69-6.406-4.354-10.511-4.354-8.209 0-14.865 6.655-14.865 14.865 0 2.732 0.737 5.291 2.022 7.491l-0.038-0.070-2.109 7.702 7.879-2.067c2.051 1.139 4.498 1.809 7.102 1.809h0.006c8.209-0.003 14.862-6.659 14.862-14.868 0-4.103-1.662-7.817-4.349-10.507l0 0zM16.062 28.228h-0.005c-2-319 0-4.489-0.64-6.342-1.753l0.056 0.031-0.451-0.267-4.675 1.227 1.247-4.559-0.294-0.467c-1.185-1.862-1.889-4.131-1.889-6.565 0-6.822 5.531-12.353 12.353-12.353s12.353 5.531 12.353 12.353c0 6.822-5.53 12.353-12.353 12.353h-0z"/></svg>
     </a>
+    @endif
+
     <button id="scrollTopBtn" class="scroll-top-float" style="display:none;">&#8593;</button>
     @stack('scripts')
 </body>
